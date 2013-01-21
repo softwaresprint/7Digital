36b4230d60da3bb16ad978f4bb8ac90bfa42cc1c
Software sprint <colinkloes@yahoo.co.uk>
2013-01-21 21:06:21 +0200

    Again add missing file InventoryItem.cs. Normally I use AnkhSVN.

---
+++ trunk/src/Shopping/InventoryItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Digital7.Shopping
+{
+    public class InventoryItem
+    {
+        public InventoryItem(string sku, double price)
+        {
+            this.Sku = sku;
+            this.Price = price;
+        }
+
+        public string Sku { get; private set; }
+
+        public double Price { get; private set; }
+    }
+}
