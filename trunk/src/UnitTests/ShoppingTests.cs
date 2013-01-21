8612d298bd0764b87a15e95bc4abfb5951080c39
Software sprint <colinkloes@yahoo.co.uk>
2013-01-21 20:00:42 +0200

    Add the Unit tests shells - Not implementation defined yet

---
+++ trunk/src/UnitTests/ShoppingTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Digital7.UnitTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ShoppingTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Total_DoNotScanAnything_Returns0()
+        {
+        }
+
+        [Test]
+        public void Total_Scan1ItemNoDiscount_ReturnsCorrectPrice()
+        {
+        }
+
+        [Test]
+        public void Total_ScanManyItemNoDiscount_ReturnsCorrectPrice()
+        {
+        }
+
+        [Test]
+        public void Total_Scan3ItemsWithDiscount_ReturnsCorrectPrice()
+        {
+        }
+
+        [Test]
+        public void Total_ScanManyAllItemsWithDiscount_ReturnsCorrectPrice()
+        {
+        }
+    }
+}
